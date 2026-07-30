7cfd20ff72c41c9cddf4ea1f122a16a991ff527e
Yuvraj Jain <yuvraj23jain@gmail.com>
2026-07-30 14:09:24 +0530

    fix: change admin apiUrl to point to the EC2 backend IP

--- admin-frontend-updated/src/environments/environment.ts
+++ admin-frontend-updated/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000',
+  apiUrl: 'http://13.204.174.51',
   dispatcherAppUrl: 'http://dispatcherside.s3-website.ap-south-1.amazonaws.com',
   agentAppUrl: 'http://localhost:4202',
   customerAppUrl: 'http://logistics-management-customer.s3-website.ap-south-1.amazonaws.com',
